7bfe24498f0c4c8a5607c3c3017b4e61447510a2
AhmedElBadry <ahmed2lbadry@gmail.com>
2018-04-30 06:47:57 +0200

    all tables are updatable

--- Simulator/assets/js/script.js
+++ Simulator/assets/js/script.js
@@ -13,14 +13,18 @@ SW reg >> mem
 var resources = {
 	adders: 2,
 	multipliers: 2,
-	LDalu: 2
+	LDalu: 2,
+	rs1: 3,
+	rs2: 3,
+	lb: 3,
+	sb: 3
 }
-var instCyc = {
-	ADD: {cyc: 2, appr: "+"},
-	SUB: {cyc: 2, appr: "-"},
-	MUL: {cyc: 2, appr: "*"},
-	LD: {cyc: 2, appr: "L"},
-	SW: {cyc: 2, appr: "S"}
+var instInfo = {
+	ADD: {cyc: 2, OP: "ADD", appr: "+"},
+	SUB: {cyc: 2, OP: "SUB", appr: "-"},
+	MUL: {cyc: 2, OP: "MUL", appr: "*"},
+	LD: {cyc: 2, OP: "LD", appr: "L"},
+	SW: {cyc: 2, OP: "SW", appr: "S"}
 };
 
 
@@ -49,11 +53,36 @@ for(let i = 0; i < numOfReg; i++){
 
 
 //reservation stations
-var ResStation1table = $("#ResStation1table");
-var ResStation1tableCont = [];
+var ResStation1Table = $("#ResStation1Table");
+var ResStation1TableCont = [];
 
-var ResStation2table = $("#ResStation2table");
-var ResStation2tableCont = [];
+var ResStation2Table = $("#ResStation2Table");
+var ResStation2TableCont = [];
+
+
+
+//load buffer
+var LoadBufferTable = $("#LoadBufferTable");
+var LoadBufferTableCont = [];
+
+//store buffer
+var StoreBufferTable = $("#StoreBufferTable");
+var StoreBufferTableCont = [];
+
+
+//initialize rs, lb and sb
+for(let i = 0; i < resources.rs1; i++){
+	ResStation1TableCont[i] = {isBusy: false, OP: "-",  rs: "-", rt: "-"};
+}
+for(let i = 0; i < resources.rs2; i++){
+	ResStation2TableCont[i] = {isBusy: false, OP: "-", rs: "-", rt: "-"};
+}
+for(let i = 0; i < resources.lb; i++){
+	LoadBufferTableCont[i] = {isBusy: false, rs: "-"};
+}
+for(let i = 0; i < resources.sb; i++){
+	StoreBufferTableCont[i] = {isBusy: false, rs: "-", rt: "-"};
+}
 
 
 //Memory
@@ -74,7 +103,6 @@ function updateRFtable() {
 	RFtable.html("");
 	for(let i = 0; i < numOfReg; i++){
 		RFtable.append("<tr><td> " + regName + i + "</td><td>" + RFtableCont[i] + "</td></tr>");
-
 	}
 }
 
@@ -86,15 +114,82 @@ function updateRATtable() {
 	}
 }
 
+function updateRS1(){
+	ResStation1Table.html("");
+	for(let i = 0; i < resources.rs1; i++){
+		ResStation1Table.append("<tr><td> " +
+			ResStation1TableCont[i].OP  + "</td><td>" +
+			ResStation1TableCont[i].rs + "</td><td>" +
+			ResStation1TableCont[i].rt +  "</td></tr>");
+	}
+}
+function updateRS2(){
+	ResStation2Table.html("");
+	for(let i = 0; i < resources.rs2; i++){
+		ResStation2Table.append("<tr><td> " +
+			ResStation2TableCont[i].OP  + "</td><td>" +
+			ResStation2TableCont[i].rs + "</td><td>" +
+			ResStation2TableCont[i].rt +  "</td></tr>");
+	}
+}
+/*
+for(let i = 0; i < resources.lb; i++){
+	LoadBufferTableCont[i] = {isBusy: false, rs: "-"};
+}
+for(let i = 0; i < resources.sb; i++){
+	StoreBufferTableCont[i] = {isBusy: false, rs: "-", rt: "-"};
+}*/
+function updateLB(){
+	LoadBufferTable.html("");
+	for(let i = 0; i < resources.lb; i++){
+		LoadBufferTable.append("<tr><td> " + LoadBufferTableCont[i].rs  + "</td></tr>");
+	}
+}
+function updateSB(){
+	StoreBufferTable.html("");
+	for(let i = 0; i < resources.lb; i++){
+		StoreBufferTable.append("<tr><td> " +
+		 StoreBufferTableCont[i].rs  + "</td><td>" +
+		 StoreBufferTableCont[i].rt + "</td></tr>");
+	}
+}
 
 
 
 
+// TO BE CONTINUED
 function issueInst(ints){
 
-	
+	//syntax for inst: OP rd, rs, rt
+	var OP = ints.substring(0, ints.indexOf(" "));
+	console.log("_"+ OP + "_");
+
+	var rd = ints.substring(ints.indexOf(" ") + 1 , ints.indexOf(", "));
+	console.log("_"+ rd + "_");
+
+	var rs = ints.substring(ints.indexOf(", ") + 2 , ints.indexOf(",", ints.indexOf(", ") + 2));
+	console.log("_"+ rs + "_");
+
+	if(OP != instInfo.LD.OP && OP != instInfo.SW.OP){
+		var rt = ints.substring(ints.lastIndexOf(", ") + 2);
+		console.log("_"+ rt + "_");
+	}
+
+
+	if(OP == instInfo.ADD.OP 
+		|| OP == instInfo.SUB.OP ){
+
+
+	}
+
+
+
+
+
 
 }
+
+issueInst("ADD F0, F2, F3");
 
 
 
@@ -119,9 +214,9 @@ function Queue(){
 			numOfElements++;
 			rear = (rear + 1) % maxNumOfElements;
 			content[rear] = el;
-			console.log("push " + el);
+			//console.log("push " + el);
 		}else{
-			console.log("queue is full");
+			//console.log("queue is full");
 			return null;
 		}
 		
@@ -131,12 +226,11 @@ function Queue(){
 		if(!this.isEmpty()){
 			numOfElements--;
 			var oldFront = content[front];
-			console.log("front " + front);
+			//console.log("front " + front);
 			front = (front + 1) % maxNumOfElements;
-			console.log("pop " + oldFront);
 			return content[oldFront];
 		}else {
-			console.log("queue is empty");
+			//console.log("queue is empty");
 		}
 		
 	}
@@ -145,7 +239,7 @@ function Queue(){
 		var arr = [];
 
 		for(let i = front; i < front + rear + 1; i++){
-			console.log(content[i]);
+			//console.log(content[i]);
 			arr.push(content[i]);
 		}
 
@@ -159,6 +253,10 @@ updateRFtable();
 
 updateRATtable();
 
+updateRS1();
+updateRS2();
+updateLB();
+updateSB();
 
 /*
 var q = new Queue();
