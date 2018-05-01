acd6c049695b9f0a8e75f9f881aa086a8615aa2e
AhmedElBadry <ahmed2lbadry@gmail.com>
2018-05-01 21:25:13 +0200

    RS1 and RS2 are ready to test

--- Simulator/assets/js/script.js
+++ Simulator/assets/js/script.js
@@ -44,7 +44,7 @@ var IQtable = $("#IQtable");
 var IQtableCont = new Queue();
 var IQ = new Queue();
 
-
+/*
 IQtableCont.push("ADD F0, F2, F3");
 IQtableCont.push("LD F0, F2(1000)");
 IQtableCont.push("ADD F0, F2, F3");
@@ -57,7 +57,7 @@ IQtableCont.push("ADD F0, F2, F3");
 
 IQ.push(
 	{	isValid: true,
-		OP: "SW",
+		OP: "ADD",
 		rd: "F0",
 		rs: "F1",
 		addrd: 0,
@@ -122,7 +122,7 @@ IQ.push(
 	}
 );
 
-
+*/
 
 //registers
 var numOfReg = 16;
@@ -193,6 +193,11 @@ var dispRS2q = new Queue();
 var dispLBq = new Queue();
 var dispSBq = new Queue();
 
+
+/*
+Write back
+*/
+var WBq = new Queue();
 
 //Memory
 var Memory = [];
@@ -270,16 +275,20 @@ function updateSB(){
 
 
 
-updateIQtable();
+function updateAll(){
+	updateIQtable();
 
-updateRFtable();
+	updateRFtable();
 
-updateRATtable();
+	updateRATtable();
 
-updateRS1();
-updateRS2();
-updateLB();
-updateSB();
+	updateRS1();
+	updateRS2();
+	updateLB();
+	updateSB();
+}
+
+updateAll();
 
 
 
@@ -297,7 +306,7 @@ updateSB();
 function issueInst(){
 	var inst = IQ.front();
 	var isAdded = false;
-	console.log(">>>  " + instInfo[inst.OP].unit)
+
 	if(instInfo[inst.OP].unit == "RS1"){
 
 		if(addInstToResSation(ResStation1TableCont, inst)){
@@ -336,6 +345,7 @@ function addInstToResSation(RS, inst){
 
 	var unit = instInfo[inst.OP].unit;
 	var isAdded = false;
+	var instString = IQtableCont.front();
 
 	if(resources[unit] < usedResources[unit]){
 		console.log("There are no " + unit + "available");
@@ -359,8 +369,9 @@ function addInstToResSation(RS, inst){
 				// 4) Tag destination reg (add to RAT)
 				RATtableCont[inst.rd] = RS[i].tag;
 
-				console.log(RS[i]);
 				usedResources[unit]++;
+
+
 			}
 		}
 
@@ -378,7 +389,6 @@ function addInstToResSation(RS, inst){
 				// 4) Tag destination reg (add to RAT)
 				RATtableCont[inst.rd] = RS[i].tag;
 
-				console.log(RS[i]);
 				usedResources[unit]++;
 			}
 		}
@@ -398,7 +408,6 @@ function addInstToResSation(RS, inst){
 				// 4) Tag destination reg (add to RAT)
 				RATtableCont[inst.rd] = RS[i].tag;
 
-				console.log(RS[i]);
 				usedResources[unit]++;
 			}
 		}
@@ -408,27 +417,24 @@ function addInstToResSation(RS, inst){
 	if(isAdded){
 		RS[i - 1].isBusy = true;
 		RS[i - 1].addedInCyc = cycleNumber;
-		console.log(RS[i - 1]);
+
+		console.log("Issue " + cycleNumber + ":  the inst: " + instString + " is added to " + unit );
+		
+	}else {
+		console.log("Issue " + cycleNumber + ":  the inst: " + instString + " is not added to " + unit );
 	}
+
 
 	return isAdded;
 
 }
 
-issueInst();
-issueInst();
-issueInst();
-issueInst();
-issueInst();
-issueInst();
+
 
 /*
 =============== END ISSUE ================
 */
 
-
-
-cycleNumber++;
 
 
 /*
@@ -471,7 +477,7 @@ function dispatch(){
 					isDispatched = true;
 				}
 
-				//add the res th dispatch queue
+				//add the res to dispatch queue
 				if(isDispatched){
 					var temp = {
 								res: res,
@@ -482,17 +488,19 @@ function dispatch(){
 					dispRS1q.push(temp);
 					usedResources.adders++;
 
+					console.log("the following inst is dispatched");
+					console.log(ResStation1TableCont[i]);
+					console.log("it's res opj");
+					console.log(temp);
+
 					//empty the RS
 					ResStation1TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS1_" + i, addedInCyc: 0};
 					updateRS1();
 				}
 				
 
-				console.log("dispatch RS1 start");
-				console.log(temp);
-				console.log("used units" + usedResources.adders);
+				console.log("used units " + usedResources.adders);
 
-				console.log("dispatch RS1 end");
 			}
 		}
 	}
@@ -500,7 +508,14 @@ function dispatch(){
 	for(let i = 0; i < resources.RS2; i++){
 
 		//is RS busy
+		
 		if(ResStation2TableCont[i].isBusy){
+			console.log("dipatch info");
+		console.log(ResStation2TableCont[i]);
+			console.log(typeof ResStation2TableCont[i].rs === 'number');
+			console.log(typeof ResStation2TableCont[i].rt === 'number');
+			console.log(resources.multipliers > usedResources.multipliers);
+			console.log(ResStation2TableCont[i].addedInCyc < cycleNumber);
 			var res = 0;
 			var isDispatched = false;
 
@@ -510,7 +525,7 @@ function dispatch(){
 			if(typeof ResStation2TableCont[i].rs === 'number'
 				&& typeof ResStation2TableCont[i].rt === 'number'
 				&& resources.multipliers > usedResources.multipliers
-				&& ResStation1TableCont[i].addedInCyc < cycleNumber){
+				&& ResStation2TableCont[i].addedInCyc < cycleNumber){
 
 				//do the operation
 				if(ResStation2TableCont[i].OP == "MUL"){
@@ -520,9 +535,10 @@ function dispatch(){
 					res = ResStation2TableCont[i].rs / ResStation2TableCont[i].rt;
 					res = res.toFixed(2);
 					isDispatched = true;
+					console.log(res);
 				}
 
-				//add the res th dispatch queue
+				//add the res to dispatch queue
 				if(isDispatched){
 					var temp = {
 								res: res,
@@ -530,28 +546,20 @@ function dispatch(){
 								addedInCyc: cycleNumber,
 								tag: ResStation2TableCont[i].tag
 							};
-					dispRS1q.push(temp);
+					dispRS2q.push(temp);
 					usedResources.multipliers++;
 
+					console.log("the following inst is dispatched");
+					console.log(ResStation2TableCont[i]);
+					console.log("it's res opj");
+					console.log(temp);
+
+					//empty the RS
+					ResStation2TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS2_" + i, addedInCyc: 0};
+					updateRS2();
 
 				}
-				var temp = {
-							res: res,
-							toBeWrittenAfter: instInfo[ResStation2TableCont[i].OP].cyc,
-							addedInCyc: cycleNumber,
-							tag: ResStation2TableCont[i].tag
-						};
-				dispRS1q.push(temp);
-				usedResources.multipliers++;
-				console.log("dispatch RS2 start");
-				console.log(temp);
-				console.log("used units" + usedResources.multipliers);
-
-				console.log("dispatch RS2 end");
-
-				//empty the RS
-				ResStation2TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS2_" + i, addedInCyc: 0};
-				updateRS2();
+				
 			}
 		}
 
@@ -570,7 +578,6 @@ function dispatch(){
 
 }
 
-dispatch();
 
 
 
@@ -580,23 +587,180 @@ dispatch();
 *********************
 =     WriteBack     =
 *********************
+
+var dispRS1q = new Queue();
+var dispRS2q = new Queue();
+var dispLBq = new Queue();
+var dispSBq = new Queue();
+WBq
+
+{
+	res: int,
+	toBeWrittenAfter: int;
+	addedInCyc: int
+}
 */
 
+
 function WriteBack(){
+
+
 	
+
+
+	//WB the first ready result
+	if(!WBq.isEmpty()){
+		var readyRes = WBq.pop();
+
+		var unit = (readyRes.tag).substring(0, (readyRes.tag).indexOf("_"));
+
+		if(unit == "RS1"){
+			usedResources.adders--;
+			console.log("an adder is released, the used: " + usedResources.adders);
+
+		}else if (unit == "RS2") {
+			usedResources.multipliers--;
+			console.log("a multipliers is released, the used: " + usedResources.multipliers);
+			
+		}else if (unit == "LB") {
+
+		}else if (unit == "SB") {
+			
+		}
+
+		captureRes(readyRes);
+	}
+
+	addReadyResToWBq(dispLBq);
+	addReadyResToWBq(dispRS2q);
+	addReadyResToWBq(dispRS1q);
+	addReadyResToWBq(dispSBq);
+
+	
+	
+}
+
+
+function addReadyResToWBq(dispRSq){
+
+	var qTemp = new Queue();
+
+	//see all results
+	while(!dispRSq.isEmpty()){
+
+		var currRes = dispRSq.pop();
+
+		var isAdded = false;
+		//add to WBq if ready
+		if(currRes.toBeWrittenAfter <= 0){
+			WBq.push(currRes);
+			isAdded = true;
+			console.log("res with tag" + currRes.tag + " is added to WB queue");
+
+		}
+
+		// if res was not ready
+		if(!isAdded){
+			//decrement the cycles
+			currRes.toBeWrittenAfter--;
+
+			//add it again to the queue
+			qTemp.push(currRes);
+		}
+
+	}
+
+	//refill RS with the not ready results
+	while (!qTemp.isEmpty()) {
+		dispRSq.push(qTemp.pop());
+	}
 }
 
 
 
 
+/*
+
+//initialize rs, lb and sb
+for(let i = 0; i < resources.RS1; i++){
+	ResStation1TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS1_" + i, addedInCyc: 0};
+}
+for(let i = 0; i < resources.RS2; i++){
+	ResStation2TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS2_" + i, addedInCyc: 0};
+}
+for(let i = 0; i < resources.LB; i++){
+	LoadBufferTableCont[i] = {isBusy: false, rs: "-", tag: "LB_" + i, addrs: 0, addedInCyc: 0};
+}
+for(let i = 0; i < resources.SB; i++){
+	StoreBufferTableCont[i] = {isBusy: false, rd: "-", rs: "-", tag: "SB_" + i, addrs: 0, addedInCyc: 0};
+}
+
+
+*/
+
+
+function captureRes(readyRes){
+
+	captureResRS(readyRes, ResStation1TableCont);
+	captureResRS(readyRes, ResStation2TableCont);
+	captureResRS(readyRes, LoadBufferTableCont);
+	captureResRS(readyRes, StoreBufferTableCont);
+
+	captureResRFandRAT(readyRes);
+
+	updateAll();
+
+
+}
+
+function captureResRS(readyRes, RS){
+	console.log("to be capture");
+	console.log(readyRes);
+	var unit = (RS[0].tag).substring(0, (RS[0].tag).indexOf("_"));
+	for(let i = 0; i < resources[unit]; i++){
+		var isCaptured = false;
+		if(RS[i].rs == readyRes.tag){
+			RS[i].rs = readyRes.res;
+			isCaptured = true;
+
+		}
+		if(RS[i].rt == readyRes.tag){
+			RS[i].rt = readyRes.res;
+			isCaptured = true;
+		} 
+		if(RS[i].rd == readyRes.tag) {
+			RS[i].rd = readyRes.res;
+			isCaptured = true;
+		}
+
+		if(isCaptured){
+			console.log("res with tag " + readyRes.tag + " in "+ unit +" is captured " );
+		}
+	}
+
+}
+
+function captureResRFandRAT(readyRes){
+
+
+	for(let i = 0; i < numOfReg; i++){
+		if(RATtableCont[regName + i] == readyRes.tag){
+			RFtableCont[regName + i] = readyRes.res;
+			RATtableCont[regName + i] = "-";
+			console.log("res with tag " + readyRes.tag + " in RF is captured " );
+		}
+		//console.log(RATtableCont[regName + i]);
+		
+	}
+
+
+}
 
 
 
 
 //will return a tag if found in RAT or a value from RF
 function findRegValue(reg){
-	console.log("2>> " + RATtableCont[reg]);
-	console.log("3>> " + reg);
 
 	return (RATtableCont[reg] == "-")? RFtableCont[reg] : RATtableCont[reg];
 }
@@ -624,7 +788,6 @@ function Queue(){
 			numOfElements++;
 			rear = (rear + 1) % maxNumOfElements;
 			content[rear] = el;
-			//console.log("push " + el);
 		}else{
 			console.log("queue is full");
 			return null;
@@ -636,9 +799,8 @@ function Queue(){
 		if(!this.isEmpty()){
 			numOfElements--;
 			var oldFront = content[front];
-			//console.log("front " + front);
 			front = (front + 1) % maxNumOfElements;
-			return content[oldFront];
+			return oldFront;
 		}else {
 			console.log("queue is empty");
 		}
@@ -657,7 +819,6 @@ function Queue(){
 		var arr = [];
 
 		for(let i = front; i < front + rear + 1; i++){
-			//console.log(content[i]);
 			arr.push(content[i]);
 		}
 
@@ -666,21 +827,15 @@ function Queue(){
 }
 
 
+
 /*
 var q = new Queue();
 
 q.push(1);
-q.push(2);
-q.push(3);
-q.push(4);
-q.push(5);
-q.push(1);
-q.push(2);
-q.push(3);
-q.push(4);
-q.push(5);
+var a = q.pop();
 
-console.log(q.retArray());
+
+console.log(a);
 */
 
 
@@ -872,22 +1027,53 @@ var instInfo = {
 
 */
 
-
 /** Sidebar functions **/
 $(document).ready(function () {
     var body = $('body'),
         sidebar = $('#sidebar'),
         sidebarForm = $('#sim-form'),
         sidebarOverlay = $('#sidebar-overlay'),
-        validationResult = true;
+        validationResult = true,
+        insInput = $('#ins'),
+        addersNum = $('#adders'),
+        mulNum = $('#mul'),
+        ldNum = $('#ld'),
+        rs1Num = $('#rs1'),
+        rs2Num = $('#rs2'),
+        lbNum = $('#lb'),
+        sbNum = $('#sb'),
+        addCyc = $('#ins-add'),
+        subCyc = $('#ins-sub'),
+        mulCyc = $('#ins-mul'),
+        divCyc = $('#ins-div'),
+        lwCyc = $('#ins-lw'),
+        swCyc = $('#ins-sw');
     
     // open & close
-    $('#menu-icon, #sidebar-overlay').on('click', function() {
+    function sidebarToggle() {
         body.add(sidebar).add(sidebarForm).toggleClass('open');
         sidebarOverlay.fadeToggle(500);
-    });
+    }
+    $('#menu-icon, #sidebar-overlay').on('click', sidebarToggle);
     
     // validation
+    // instructions
+    var instructions;
+    function instValidation() {
+        instructions = insInput.val().split("\n");
+        $.each(instructions, function(n, elem) {
+            if( isInstValid(elem).isValid === false ){
+                insInput.addClass('wrong');
+                console.log("instruction " + (n+1) +" is wrong");
+                validationResult = false;
+            } else {
+                insInput.removeClass('wrong');
+            }
+        } );
+    }
+    insInput.on('blur', instValidation);
+    
+    // other inputs
     function validateInput() {
         if ( !$.isNumeric($(this).val()) || $(this).val() < 0 ){
             $(this).addClass('wrong');
@@ -898,13 +1084,92 @@ $(document).ready(function () {
     }
     $('.pos-num').on('blur', validateInput);
     
+    
     $('#form-submit').on('click', function() {
         validationResult = true;
+        instValidation();
         $('.pos-num').each( validateInput );
+        
+        // if everything is okay
         if (validationResult === true){
-            // put function here
+            // emptying queues
+            while( !IQtableCont.isEmpty() ){
+                IQtableCont.pop();
+            }
+            while( !IQ.isEmpty() ){
+                IQ.pop();
+            }
+            
+            // insert instructions in ret objects into queues
+            $.each(instructions, function(n, elem) {
+                IQtableCont.push(elem);
+                IQ.push( isInstValid(elem) );
+            });
+            
+            // update resources values
+            resources.adders = addersNum.val();
+            resources.multipliers = mulNum.val();
+            resources.LDalu = ldNum.val();
+            resources.RS1 = rs1Num.val();
+            resources.RS2 = rs2Num.val();
+            resources.LB = lbNum.val();
+            resources.SB = sbNum.val();
+            
+            // update inst cycles value
+            instInfo.ADD.cyc = addCyc.val();
+            instInfo.SUB.cyc = subCyc.val();
+            instInfo.MUL.cyc = mulCyc.val();
+            instInfo.DIV.cyc = divCyc.val();
+            instInfo.LD.cyc = lwCyc.val();
+            instInfo.SW.cyc = swCyc.val();
+            
+            // close sidebar
+            setTimeout(sidebarToggle, 1000);
+
+            console.log(IQtableCont);
+            console.log(IQ);
+            console.log(resources);
+            console.log(instInfo);
+            updateAll();
         }
     });
     
 });
+
+
+var controller = 0;
+
+
+function runAcyc(){
+
+	if(controller == 0){
+		console.log("start cycle " + cycleNumber);
+		console.log("cycle " + cycleNumber + ": issue start");
+		if(!IQ.isEmpty()){
+			issueInst();
+		}else {
+			console.log("There is no instructions to issue");
+		}
+		
+		controller++;
+		console.log("cycle " + cycleNumber + ": issue end");
+
+	}else if(controller == 1) {
+		console.log("cycle " + cycleNumber + ": dispatch start");
+		dispatch();
+		controller++;
+		console.log("cycle " + cycleNumber + ": dispatch end");
+	}else if(controller == 2) {
+		console.log("cycle " + cycleNumber + ": WriteBack start");
+		WriteBack();
+		controller = 0;
+		console.log("cycle " + cycleNumber + ": WriteBack end");
+		console.log("end cycle " + cycleNumber);
+
+		//start new cycle
+		cycleNumber++;
+	}
+
+	
+}
 
