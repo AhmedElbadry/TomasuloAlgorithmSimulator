502045cb962224cd654987ce7fbae6d7a9874047
AhmedElBadry <ahmed2lbadry@gmail.com>
2018-05-01 05:39:18 +0200

    issue function is ready

--- Simulator/assets/js/script.js
+++ Simulator/assets/js/script.js
@@ -8,8 +8,9 @@ SW reg >> mem
 
 */
 
-
+var cycleNumber = 1;
 //hardware resources
+
 var resources = {
 	adders: 2,
 	multipliers: 2,
@@ -39,14 +40,80 @@ var instInfo = {
 
 var IQtable = $("#IQtable");
 var IQtableCont = new Queue();
+var IQ = new Queue();
+
+
 IQtableCont.push("ADD F0, F2, F3");
 IQtableCont.push("LD F0, F2(1000)");
 IQtableCont.push("ADD F0, F2, F3");
+IQtableCont.push("ADD F0, F2, F3");
+IQtableCont.push("LD F0, F2(1000)");
+IQtableCont.push("ADD F0, F2, F3");
+IQtableCont.push("ADD F0, F2, F3");
+IQtableCont.push("LD F0, F2(1000)");
+IQtableCont.push("ADD F0, F2, F3");
+
+IQ.push(
+	{	isValid: true,
+		OP: "SW",
+		rd: "F0",
+		rs: "F1",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	}
+);
+
+IQ.push(
+	{	isValid: true,
+		OP: "LD",
+		rd: "F1",
+		rs: "F2",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	}
+);
+IQ.push(
+	{	isValid: true,
+		OP: "ADD",
+		rd: "F2",
+		rs: "F3",
+		rt: "F4",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	}
+);
+IQ.push(
+	{	isValid: true,
+		OP: "SUB",
+		rd: "F3",
+		rs: "F3",
+		rt: "F4",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	}
+);
+
+IQ.push(
+	{	isValid: true,
+		OP: "MUL",
+		rd: "F4",
+		rs: "F3",
+		rt: "F4",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	}
+);
 
 
 //registers
 var numOfReg = 16;
 var regName = "F";
+var regName2 = "R";
 var RFtable = $("#RFtable");
 var RFtableCont = [];
 
@@ -59,7 +126,7 @@ var RATtableCont = [];
 
 //initialize RF and RAT
 for(let i = 0; i < numOfReg; i++){
-	RFtableCont[regName + i] = 0;
+	RFtableCont[regName + i] = i;
 	RATtableCont[regName + i] = "-";
 }
 
@@ -84,16 +151,16 @@ var StoreBufferTableCont = [];
 
 //initialize rs, lb and sb
 for(let i = 0; i < resources.RS1; i++){
-	ResStation1TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS1_" + i};
+	ResStation1TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS1_" + i, addedInCyc: 0};
 }
 for(let i = 0; i < resources.RS2; i++){
-	ResStation2TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS2_" + i};
+	ResStation2TableCont[i] = {isBusy: false, OP: "-", appr: "-", rs: "-", rt: "-", tag: "RS2_" + i, addedInCyc: 0};
 }
 for(let i = 0; i < resources.LB; i++){
-	LoadBufferTableCont[i] = {isBusy: false, rs: "-", tag: "LB_" + i};
+	LoadBufferTableCont[i] = {isBusy: false, rs: "-", tag: "LB_" + i, addrs: 0, addedInCyc: 0};
 }
 for(let i = 0; i < resources.SB; i++){
-	StoreBufferTableCont[i] = {isBusy: false, rs: "-", rt: "-", tag: "SB_" + i};
+	StoreBufferTableCont[i] = {isBusy: false, rd: "-", rs: "-", tag: "SB_" + i, addrs: 0, addedInCyc: 0};
 }
 
 
@@ -107,7 +174,7 @@ function updateIQtable () {
 	IQtable.html("");
 	var IQtableContArr = IQtableCont.retArray();
 	IQtableContArr.forEach(function(el) {
-		IQtable.prepend("<tr><td>" + el + "</td></tr>");
+		IQtable.prepend("<tr><td>" + ((el)? el : "-") + "</td></tr>");
 	});
 }
 
@@ -162,10 +229,13 @@ function updateSB(){
 	StoreBufferTable.html("");
 	for(let i = 0; i < resources.SB; i++){
 		StoreBufferTable.append("<tr><td> " +
-		 StoreBufferTableCont[i].rs  + "</td><td>" +
-		 StoreBufferTableCont[i].rt + "</td></tr>");
+		 StoreBufferTableCont[i].rd  + "</td><td>" +
+		 StoreBufferTableCont[i].rs + "</td></tr>");
 	}
 }
+
+
+
 
 
 
@@ -178,95 +248,173 @@ function updateSB(){
 2) Get inputs from RF ot RAT
 3) Issue the inst in a free RS
 4) Tag destination reg (add to RAT)
-
-
-
-
-
 */
-// TO BE CONTINUED
-function issueInst(inst){
 
-/*
-	//syntax for inst: OP rd, rs, rt
-	var OP = inst.substring(0, inst.indexOf(" "));
-	//console.log("_"+ OP + "_");
-
-	var rd = inst.substring(inst.indexOf(" ") + 1 , inst.indexOf(", "));
-	//console.log("_"+ rd + "_");
-
-	var rs = inst.substring(inst.indexOf(", ") + 2 , inst.indexOf(",", inst.indexOf(", ") + 2));
-	//console.log("_"+ rs + "_");
-
-	if(OP != instInfo.LD.OP && OP != instInfo.SW.OP){
-		var rt = inst.substring(inst.lastIndexOf(", ") + 2);
-		//console.log("_"+ rt + "_");
-	}*/
-
-
+function issueInst(){
+	var inst = IQ.front();
+	var isAdded = false;
+	console.log(">>>  " + instInfo[inst.OP].unit)
 	if(instInfo[inst.OP].unit == "RS1"){
 
 		if(addInstToResSation(ResStation1TableCont, inst)){
 			updateRS1();
-			updateRATtable();
+			isAdded = true;
+		}
+	}else if(instInfo[inst.OP].unit == "RS2"){
+		if(addInstToResSation(ResStation2TableCont, inst)){
+			updateRS2();
+			isAdded = true;
+		}
+	}else if (instInfo[inst.OP].unit == "LB"){
+		
+		if(addInstToResSation(LoadBufferTableCont, inst)){
+			updateLB();
+			isAdded = true;
+		}
 
+	}else if (instInfo[inst.OP].unit == "SB") {
+			if(addInstToResSation(StoreBufferTableCont, inst)){
+			updateSB();
+			isAdded = true;
 		}
 	}
 
+	if(isAdded){
+		IQtableCont.pop();
+		IQ.pop();
 
-	
+		updateIQtable();
+		updateRATtable();
+	}
 
 }
-
 function addInstToResSation(RS, inst){
 
 	var unit = instInfo[inst.OP].unit;
-	if(unit == "RS1" || unit == "RS2" ){
-		let isAdded = false;
+	var isAdded = false;
 
-		if(resources[unit] > usedResources[unit]){
-			for(let i = 0; i < resources[unit] && !isAdded; i++){
-				if(!RS[i].isBusy){
-					isAdded = true;
-
-					// 2) Get inputs from RF ot RAT
-					// 3) Issue the inst in a free RS
-
-					RS[i].isBusy = true;
-					RS[i].OP = inst.OP;
-					RS[i].appr = instInfo[inst.OP].appr;
-					RS[i].rs = findRegValue(inst.rs);
-					RS[i].rt = findRegValue(inst.rt);
-					/*
-					RS[i] = {isBusy: true,
-						OP: inst.OP,
-						appr: instInfo[inst.OP].appr,
-						rs: findRegValue(inst.rs),
-						rt: findRegValue(inst.rt),
-					};*/
-
-					// 4) Tag destination reg (add to RAT)
-					RATtableCont[inst.rd] = RS[i].tag;
-
-					console.log(RS[i]);
-					usedResources[unit]++;
-				}
-			}
-		}else {
-			console.log("All " + unit + " are used")
-		}
-		
-		return isAdded;
+	if(resources[unit] < usedResources[unit]){
+		console.log("There are no " + unit + "available");
+	 	return false;
 	}
 
+	if(unit == "RS1" || unit == "RS2" ){
+
+		for(var i = 0; i < resources[unit] && !isAdded; i++){
+			if(!RS[i].isBusy){
+				isAdded = true;
+
+				// 2) Get inputs from RF ot RAT
+				// 3) Issue the inst in a free RS
+
+				RS[i].OP = inst.OP;
+				RS[i].appr = instInfo[inst.OP].appr;
+				RS[i].rs = findRegValue(inst.rs);
+				RS[i].rt = findRegValue(inst.rt);
+
+				// 4) Tag destination reg (add to RAT)
+				RATtableCont[inst.rd] = RS[i].tag;
+
+				console.log(RS[i]);
+				usedResources[unit]++;
+			}
+		}
+
+	}else if (unit == "LB"){
+		for(var i = 0; i < resources[unit] && !isAdded; i++){
+			if(!RS[i].isBusy){
+				isAdded = true;
+
+				// 2) Get inputs from RF ot RAT
+				// 3) Issue the inst in a free RS
+
+				RS[i].rs = findRegValue(inst.rs);
+				RS[i].addrs = inst.addrs;
+
+				// 4) Tag destination reg (add to RAT)
+				RATtableCont[inst.rd] = RS[i].tag;
+
+				console.log(RS[i]);
+				usedResources[unit]++;
+			}
+		}
+	}else if(unit == "SB"){
+		for(var i = 0; i < resources[unit] && !isAdded; i++){
+			if(!RS[i].isBusy){
+				isAdded = true;
+
+				// 2) Get inputs from RF ot RAT
+				// 3) Issue the inst in a free RS
+
+				
+				RS[i].rd = findRegValue(inst.rs);
+				RS[i].rs = findRegValue(inst.rs);
+				RS[i].addrs = inst.addrs;
+
+				// 4) Tag destination reg (add to RAT)
+				RATtableCont[inst.rd] = RS[i].tag;
+
+				console.log(RS[i]);
+				usedResources[unit]++;
+			}
+		}
+
+	}
+
+	if(isAdded){
+		RS[i - 1].isBusy = true;
+		RS[i - 1].addedInCyc = cycleNumber;
+		console.log(RS[i - 1]);
+	}
+
+	return isAdded;
+
 }
-var tempInst = {isValid: true,
-	OP: "ADD",
-	rd: "F0",
-	rs: "F1",
-	rt: "F2"
+
+issueInst();
+issueInst();
+issueInst();
+issueInst();
+issueInst();
+
+/*
+=============== END ISSUE ================
+*/
+
+
+
+cycleNumber++;
+
+
+/*
+*******************
+=     DISPATCH     =
+*******************
+*/
+
+function dispatch(){
+	
 }
-issueInst(tempInst);
+
+
+
+
+
+/*
+*******************
+=     CAPTURE     =
+*******************
+*/
+
+function capture(){
+
+}
+
+
+
+
+
+
 
 
 //will return a tag if found in RAT or a value from RF
@@ -302,7 +450,7 @@ function Queue(){
 			content[rear] = el;
 			//console.log("push " + el);
 		}else{
-			//console.log("queue is full");
+			console.log("queue is full");
 			return null;
 		}
 		
@@ -316,9 +464,17 @@ function Queue(){
 			front = (front + 1) % maxNumOfElements;
 			return content[oldFront];
 		}else {
-			//console.log("queue is empty");
+			console.log("queue is empty");
 		}
 		
+	}
+
+	this.front = function(){
+		if(!this.isEmpty()){
+			return content[front];
+		}else {
+			console.log("queue is empty");
+		}
 	}
 
 	this.retArray = function(){
@@ -373,7 +529,6 @@ console.log(sum(5 , 4));*/
 
 
 
-
 /*
 
 ALLOWED syntax
@@ -396,7 +551,121 @@ return true if the input string is one of the formats above, return false otherw
 
 */
 function isInstValid(inst){
+	var ret = {
+		isValid: false,
+		OP: "",
+		rd: "",
+		rs: "",
+		rt: "",
+		addrd: 0,
+		addrs: 0,
+		addrt: 0
+	};
+
+	var myins;
+	var counter = -1;
+	var temp = inst.substring(0, inst.indexOf(" "));
+	var temp2;
+	var new_ins = inst.substring(inst.indexOf(" ") + 1);
+	var RorF;
+	var open = false;
+
+	for(var instType in instInfo){
+		if(instInfo[instType].OP == temp){
+			ret.OP = temp;
+			myins = instType;
+			break;
+		}
+	}
+
+	if(ret.OP !== temp){
+		console.log("instruction is not valid\n");
+		return ret;
+	}
+
+	while(true){
+		counter++;
+        if(new_ins.indexOf(" ") == -1)
+        	temp = new_ins;
+        else
+        	temp = new_ins.substring(0 , new_ins.indexOf(" "));
+
+        if(temp[0] == '('){
+        	open = true;
+	        temp = temp.substring(1);
+        }
+
+        if(temp[0] !== regName && temp[0] !== regName2 && '0123456789'.indexOf(temp[0]) == -1){
+        	console.log("Register name is not valid\n");
+        	return ret;
+        }
+
+        if(temp[0] == regName || temp[0] == regName2){
+        	RorF = temp[0];
+	        temp = temp.substring(1);
+	        temp2 = "";
+	        while('0123456789'.indexOf(temp[0]) !== -1){
+	        	temp2 += temp[0];
+	        	temp = temp.substring(1);
+	        }
+
+	        if( (parseInt(temp2) >= numOfReg) || ((temp[0] == ')') !== open) || (temp !== "," && temp !== "" && !open) || (temp == "," && new_ins.indexOf(" ") == -1) || (temp == "" && new_ins.indexOf(" ") !== -1) ){
+	        	console.log("Instruction Error!\n");
+	        	return ret;
+	        }
+
+	        open = false;
+
+	        if(!counter)
+	        	ret.rd = RorF + temp2;
+	        else if (counter == 1)
+	        	ret.rs = RorF + temp2;
+	        else
+	        	ret.rt = RorF + temp2;
+        }
+
+        else if('0123456789'.indexOf(temp[0]) !== -1){
+	        temp2 = "";
+	        while('0123456789'.indexOf(temp[0]) !== -1){
+	        	temp2 += temp[0];
+	        	temp = temp.substring(1);
+	        }
+
+	        if( ((temp[0] == ')') !== open) || (temp !== "," && temp !== "" && !open && temp[0] !== '(') || (temp == "," && new_ins.indexOf(" ") == -1) || (temp == "" && new_ins.indexOf(" ") !== -1) ){
+	        	console.log("Instruction Error!\n");
+	        	return ret;
+	        }
+
+	        open = false;
+
+	        if(!counter)
+	        	ret.addrd = parseInt(temp2);
+	        else if (counter == 1)
+	        	ret.addrs = parseInt(temp2);
+	        else
+	        	ret.addrt = parseInt(temp2);
+
+	        if(temp[0] == '('){
+	        	new_ins = new_ins.substring(new_ins.indexOf('('));
+	        	counter--;
+	        	continue;
+	        }
+        }
+
+        if(new_ins.indexOf(" ") == -1)
+        	break;
+        else
+        	new_ins = new_ins.substring(new_ins.indexOf(" ") + 1);
+	}
+
+	if(instInfo[myins].nOperands !== counter){
+        	console.log("operands count error!\n");
+        	return ret;
+	}
+
+	ret.isValid = true;
+	return ret;
 }
 
-
+console.log(isInstValid("ADD F0, F2, F3"));
 
